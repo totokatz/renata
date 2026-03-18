baebd88d7d3ffe95235977334ebfeb0d3bc203ea
totokatz <tototomas2002@hotmail.com.ar>
2026-03-18 19:32:38 -0300

    feat: add AnimatePresence wrapper for page transitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
 import Home from './pages/Home'
+import ArtworkDetail from './pages/ArtworkDetail'
+import Collections from './pages/Collections'
+
+function AnimatedRoutes() {
+  const location = useLocation()
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/collections" element={<Collections />} />
+        <Route path="/artwork/:id" element={<ArtworkDetail />} />
+      </Routes>
+    </AnimatePresence>
+  )
+}
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <AnimatedRoutes />
     </BrowserRouter>
   )
 }
